3870e018b51242970673b223ac7067bd6604442a
Garima19dec <garima19dec1996@gmail.com>
2022-04-09 21:30:01 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Apps from "./Apps";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Apps />,
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
